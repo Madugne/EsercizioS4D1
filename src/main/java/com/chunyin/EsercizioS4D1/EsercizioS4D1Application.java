0cf6f5209aae4c6f909a15f1668ecd3f880d18b6
Fabio Yu <fabioyu7665@gmail.com>
2024-04-02 13:55:41 +0200

    Initial commit

---
+++ src/main/java/com/chunyin/EsercizioS4D1/EsercizioS4D1Application.java
@@ -0,0 +1,13 @@
+package com.chunyin.EsercizioS4D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioS4D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioS4D1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/chunyin/EsercizioS4D1/EsercizioS4D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chunyin.EsercizioS4D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioS4D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
